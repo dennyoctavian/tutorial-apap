1324a63fa78352c590f3a2967144684f06b69944
Denny Octavian <dennyoctavian164@gmail.com>
2021-09-08 15:36:04 +0700

    feat: tutorial 1 - initialize spring boot project

---
+++ IsPalindrom/src/main/java/apap/tutorial/IsPalindrom/IsPalindromApplication.java
@@ -0,0 +1,13 @@
+package apap.tutorial.IsPalindrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsPalindromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsPalindromApplication.class, args);
+	}
+
+}
