ae689197ab14ed17bf63114a57ed0bd365fa2992
Denny Octavian <dennyoctavian164@gmail.com>
2021-11-30 11:49:29 +0700

    demo lab 7 bersama kak mudrik

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
 import "./views/Home/index";
 import Home from './views/Home/index';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import React, { useState } from 'react';
+
+
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    // if the theme is not light, then set it to dark
+    if (theme === 'light') {
+      setTheme('dark');
+    // otherwise, it should be light
+    } else {
+      setTheme('light');
+    }
+  }
   return (
+    
     <div className="App">
+      <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
+        <button className="btn btn-primary" onClick={toggleTheme}>Toggle theme</button>
         <Home />
+      </ThemeProvider>
+      
     </div>
   );
 }
